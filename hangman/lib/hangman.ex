1782de9915bbfc43eb6dd43cbcf7cea5a0de3587
PedroHamamoto <hamamoto.pedro@gmail.com>
2024-06-06 17:51:24 -0700

    Handling guess when state is won or lost

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,4 +3,7 @@ defmodule Hangman do
 
   @spec new_game :: Game.t
   defdelegate new_game, to: Game
+
+  @spec make_move(Game.t, String.t) :: { Game.t, Type.tally }
+  defdelegate make_move(game, guess), to: Game
 end
--- hangman/test/impl/game_test.exs
+++ hangman/test/impl/game_test.exs
@@ -5,12 +5,24 @@ defmodule Hangman.Impl.GameTest do
   alias Hangman.Impl.Game
 
   test "should create a new game" do
-    game = Game.new_game
+    game = Game.new_game("life")
 
     assert game.turns_left == 7
     assert game.game_state == :initializing
-    assert length(game.letters) > 1
+    assert game.letters == ["l", "i", "f", "e"]
     assert game.used == MapSet.new()
   end
+
+  test "should not change state when state is won | lost" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game()
+      game = %{ game | game_state: state }
+      { new_game, _tally } = Game.make_move(game, "x")
+
+      assert game == new_game
+    end
+  end
+
+
 
 end
